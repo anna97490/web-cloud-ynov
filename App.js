44cac84077d8d4c6f0a51582791adda8043282da
anna97490 <anna.cousin97490@gmail.com>
2024-03-28 11:51:27 +0100

    form rules

--- App.js
+++ App.js
@@ -8,26 +8,92 @@ export default function App() {
   const [email, onChangeEmail] = React.useState('');
   const [password, onChangePassword] = React.useState('');
 
+  // Regex email
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  // Regex password
+  // At least one upper case letter, one lower case letter, one digit, 6-20 characters
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+  // function to check the regex email
+  const checkEmail = (email) => {
+    return emailRegex.test(email);
+  };
+
+  // function to check the regex password
+  const checkPassword = (password) => {
+    return passwordRegex.test(password);
+  };
+
+  const handleSignup = () => {
+    if (checkEmail(email) && checkPassword(password)) {
+      signup(email, password);
+      alert("User created successfully");
+    }
+    else {
+      alert("Invalid email or password");
+    }
+  }
+
+  const handleSignin = () => {
+    if (checkEmail(email) && checkPassword(password)) {
+      signin(email, password);
+      alert("Signin success");
+    }
+    else {
+      alert("Invalid email or password");
+    }
+  }
 
   return (
     <View style={styles.container}>
       <Text>Email</Text>
       <TextInput
         style={styles.input}
-        onChangeText={onChangeEmail}
+        onChangeText={text=>onChangeEmail(text)}
         value={email}
       ></TextInput>
       <Text>Password</Text>
       <TextInput
         style={styles.input}
-        onChangeText={onChangePassword}
+        onChangeText={text=>onChangePassword(text)}
+        value={password}
+        secureTextEntry={true}
+      ></TextInput>
+      <Button title="Sign up" onPress={handleSignup}></Button>
+      <Button title="Sign In" onPress={handleSignin}></Button>
+    </View>
+  );
+
+    /*return (
+    <View style={styles.container}>
+      <Text>Email</Text>
+      <TextInput
+        style={styles.input}
+        value={email}
+        // Regex email
+        onChangeText={text => {
+          if (emailRegex.test(text)) {
+            onChangeEmail(text);
+          }
+        }}
+      ></TextInput>
+      <Text>Password</Text>
+      <TextInput
+        style={styles.input}
+         // Regex password
+         onChangeText={text => {
+          if (passwordRegex.test(text)) {
+            onChangePassword(text);
+          }
+        }}
         value={password}
         secureTextEntry={true}
       ></TextInput>
       <Button title="Sign up" onPress={() => signup(email, password)}></Button>
       <Button title="Sign In" onPress={() => signin(email, password)}></Button>
     </View>
-  );
+  );*/
 }
 
 const styles = StyleSheet.create({
